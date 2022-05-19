c9b1a903227e331b100b7990a57080da6d3aa897
nikolai-in <nikolai.in@tuta.io>
2022-05-19 12:17:34 +0300

    :lipstick: Update UI

--- app/src/Rects/RectRoom.gd
+++ app/src/Rects/RectRoom.gd
@@ -1,23 +1,16 @@
-extends Label
-
+extends Polygon2D
+class_name RectRoom
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
-onready var polygon : PoolVector2Array = get_parent().polygon 
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rect_position = (polygon[0]+polygon[2])/2 - rect_size/2
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
-
-
-func _on_Rect_renamed() -> void:
-	rect_position = (polygon[0]+polygon[2])/2 - rect_size/2
-	text = owner.name
--- app/src/UI/UI.gd
+++ app/src/UI/UI.gd
@@ -1,16 +1,14 @@
-extends Label
+extends CanvasLayer
 
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
-onready var polygon : PoolVector2Array = get_parent().polygon 
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rect_position = (polygon[0]+polygon[2])/2 - rect_size/2
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +16,8 @@ func _ready() -> void:
 #	pass
 
 
-func _on_Rect_renamed() -> void:
-	rect_position = (polygon[0]+polygon[2])/2 - rect_size/2
-	text = owner.name
+func _on_Button_toggled(button_pressed: bool) -> void:
+	if button_pressed: 
+		$Popup.popup()
+	else:
+		$Popup.hide()
